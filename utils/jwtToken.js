eae45d23b729ab4067475a268b240816919e1338
sahzadahmad246 <124631079+sahzadahmad246@users.noreply.github.com>
2024-10-23 11:00:54 +0530

    Update jwtToken.js changes same site to lax

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
     httpOnly: true,
-    sameSite: 'None',
+    sameSite: 'lax',
     secure: process.env.NODE_ENV === 'production', 
   };
 
