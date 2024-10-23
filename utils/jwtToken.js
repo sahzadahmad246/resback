c33818f43c0ddb2e05c7503fe4ce8e44a2cd4353
sahzadahmad246 <124631079+sahzadahmad246@users.noreply.github.com>
2024-10-23 11:35:12 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
     httpOnly: true,
     sameSite: 'none',
-    secure: process.env.NODE_ENV === 'production', 
+    secure: true, 
   };
 
   res.status(statusCode).cookie("token", token, options).json({
