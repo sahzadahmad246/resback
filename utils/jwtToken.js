757b2c04a0b8d6b3060bebdd73f40df5528614b6
Shahzad Ahmad <sahzadahmad246@gmail.com>
2024-11-03 16:20:02 +0530

    trying to fix cookie issue

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,13 +1,11 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-  
   const options = {
-    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'none',
-    secure: true,
-    partitioned: true,
+    sameSite: 'None',
+    secure: true, 
   };
 
   res.status(statusCode).cookie("token", token, options).json({
