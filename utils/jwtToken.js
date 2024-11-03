272c620bf172baeb897bc5162e0ada1cde622dd7
Shahzad Ahmad <sahzadahmad246@gmail.com>
2024-11-03 16:47:00 +0530

    dhdh

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,11 +2,13 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
   const options = {
-    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
     httpOnly: true,
-    secure: true, // Ensure secure is true for cross-site cookies
-    // Remove sameSite and partitioned options to test compatibility
-    domain: 'https://resback-ql89.onrender.com', // Set explicitly to your backend's domain
+    secure: true,
+    domain: "https://resback-ql89.onrender.com",
+    sameSite: "none",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
