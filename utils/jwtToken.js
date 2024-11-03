70b4bebe741bbf7a89ad61496106b5c71a116d27
Shahzad Ahmad <sahzadahmad246@gmail.com>
2024-11-03 16:28:24 +0530

    fixing cookie issue

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,8 +4,9 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'None',
-    secure: true, 
+    secure: true, // Ensure secure is true for cross-site cookies
+    // Remove sameSite and partitioned options to test compatibility
+    domain: 'https://resback-ql89.onrender.com', // Set explicitly to your backend's domain
   };
 
   res.status(statusCode).cookie("token", token, options).json({
