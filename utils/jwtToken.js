4932d97433fb4004710a706e98d0535f0c55a72f
Shahzad Ahmad <sahzadahmad246@gmail.com>
2024-11-14 18:13:15 +0530

    ghfd

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,7 +6,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "None",
     secure: true,
     path: "/",
   };
